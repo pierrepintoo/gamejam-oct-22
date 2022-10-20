4ffcef3c3b700ec221d80f83fa5977f8c4d60330
Moreau <guen94@hotmail.fr>
2022-10-20 17:43:48 +0200

    add game over page and start notice page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Game from './components/Game/Game';
-import GameOver from './components/GameOver/GameOver.jsx';
+import GameOver from './components/GameOver/GameOver';
+import Notice from './components/Notice/Notice';
 import { useState } from 'react';
 
 function App() {
@@ -23,7 +24,8 @@ function App() {
       {/* <Game 
         mousePos={mousePos}
       /> */}
-      <GameOver/>
+      {/* <GameOver/> */}
+      <Notice/>
     </div>
   );
 }
